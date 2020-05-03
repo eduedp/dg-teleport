d513f7fee1645136f3d6dc926cec7bc72ffeef79
eduedp <8978381+eduedp@users.noreply.github.com>
2020-05-03 02:10:41 -0300

    Minor update Patch 93

--- index.js
+++ index.js
@@ -6,46 +6,54 @@ module.exports = function DGTeleport(mod) {
     let teleportList = []
 
     mod.dispatch.addDefinition('C_REQUEST_EVENT_MATCHING_TELEPORT', 0, path.join(__dirname, 'C_REQUEST_EVENT_MATCHING_TELEPORT.0.def'));
-    mod.dispatch.addDefinition('S_AVAILABLE_EVENT_MATCHING_LIST', 0, path.join(__dirname, 'S_AVAILABLE_EVENT_MATCHING_LIST.0.def'));
 
-    mod.hook('S_AVAILABLE_EVENT_MATCHING_LIST', 0, (e) => {
-        // Get available Dungeon events id -- type 0
-        const newAvailableEvents = e.quests.filter(x => x.type == 0).map(x => x.id)
+    mod.setTimeout(() => {
+        const valid = mod.dispatch.protocolMap.name.get('C_REQUEST_EVENT_MATCHING_TELEPORT');
+        if (valid === undefined || valid == null) {
+            mod.error(`Could not load DG-Teleport. Missing C_REQUEST_EVENT_MATCHING_TELEPORT in the protocol map.`);
+        }
+    }, 1000)
+
+    mod.hook('S_AVAILABLE_EVENT_MATCHING_LIST', 2, (e) => {
+        // Get available Dungeon events id -- type 0 (unk2)
+        const newAvailableEvents = e.quests.filter(x => x.unk2 == 0).map(x => x.id)
         if (newAvailableEvents.join('') == availableEvents.join('')) return
         
         availableEvents = newAvailableEvents
         teleportList = []
 
-        mod.queryData('/EventMatching/EventGroup/Event@type=?', ['Dungeon'], true).then((events) => {    
-            // Filter only available and sort by ilvl 
-            events = events
-                .filter((x) => availableEvents.includes(x.attributes.id))
-                .sort((b, a) => a.attributes.requiredItemLevel - b.attributes.requiredItemLevel || a.attributes.id - b.attributes.id)
-
-            // Calc ilvl colors           
-            let colors = new Map();
-            const ilvls = [... new Set(events.map(x => x.attributes.requiredItemLevel))]
-            ilvls.forEach((ilvl, x) => colors.set(ilvl, ALL_COLORS[x]))
-            
-            events.forEach((event) => {    
-                const zoneId = event.children.find(x => x.name == "TargetList").children.find(x => x.name == "Target").attributes.id
+        mod.setTimeout(() => {
+            mod.queryData('/EventMatching/EventGroup/Event@type=?', ['Dungeon'], true).then((events) => {    
+                // Filter only available and sort by ilvl 
+                events = events
+                    .filter((x) => availableEvents.includes(x.attributes.id))
+                    .sort((b, a) => a.attributes.requiredItemLevel - b.attributes.requiredItemLevel || a.attributes.id - b.attributes.id)
     
-                mod.queryData('/StrSheet_Dungeon/String@id=?', [zoneId]).then((dungeon) => {   
-                    // Calc acronyms and iLvl color 
-                    const regex = / |of|\(.*\)/i
-                    const acronym = dungeon.attributes.string.split(regex).map(x => x.charAt(0).toLowerCase()).join('')
-
-                    teleportList.push({
-                        eventId: event.attributes.id,
-                        dungeonName: dungeon.attributes.string,
-                        acronyms: [ acronym, acronym+'n', acronym+'h' ],
-                        requiredItemLevel: event.attributes.requiredItemLevel,
-                        zoneId: zoneId,
-                        color: colors.get(event.attributes.requiredItemLevel)
-                    })
-                });
-            })
-        });
+                // Calc ilvl colors           
+                let colors = new Map();
+                const ilvls = [... new Set(events.map(x => x.attributes.requiredItemLevel))]
+                ilvls.forEach((ilvl, x) => colors.set(ilvl, ALL_COLORS[x]))
+                
+                events.forEach((event) => {    
+                    const zoneId = event.children.find(x => x.name == "TargetList").children.find(x => x.name == "Target").attributes.id
+        
+                    mod.queryData('/StrSheet_Dungeon/String@id=?', [zoneId]).then((dungeon) => {   
+                        // Calc acronyms and iLvl color 
+                        const regex = / |of|\(.*\)/i
+                        const acronym = dungeon.attributes.string.split(regex).map(x => x.charAt(0).toLowerCase()).join('')
+    
+                        teleportList.push({
+                            eventId: event.attributes.id,
+                            dungeonName: dungeon.attributes.string,
+                            acronyms: [ acronym, acronym+'n', acronym+'h' ],
+                            requiredItemLevel: event.attributes.requiredItemLevel,
+                            zoneId: zoneId,
+                            color: colors.get(event.attributes.requiredItemLevel)
+                        })
+                    });
+                })
+            });
+        }, 5000)
     });
 
     cmd.add('dg', (value) => {
